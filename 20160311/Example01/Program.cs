064a203b374fd9679cfc61803ff7444234d20716
maybesome <wuarthur88@gmail.com>
2016-03-11 22:05:20 +0800

    SGS

---
+++ 20160311/Example01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example01
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Student 老吳 =
+                new Student
+                {
+                    name = "arthur",
+                    email = "qq@gmail.com",
+                    phone = "0912345678"
+                };
+            Console.WriteLine(老吳.name);
+            Console.WriteLine(老吳.email);
+            Console.WriteLine(老吳.phone);
+        }
+    }
+}
---
+++ 20160311/Example01/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example01
+{
+    internal class Student
+    {
+        public string name;
+        new Contact
+            {
+                public string email;
+                public string phone;
+            }
+    }
+}
